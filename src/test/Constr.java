9b9107b353a0c0de6a3fcfade031f55dbeadbc08
Amine1044 <117497865+Amine1044@users.noreply.github.com>
2023-10-25 21:43:10 -0500

    first commit

---
+++ src/test/Constr.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Constr {
+
+		String a;
+		static String b="5";
+		public Constr (String c) {
+			this.b=c;
+			System.out.println(b);
+		}
+		
+
+	
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(djihed(657,65648));
+		
+	
+	}
+	
+	public static int djihed(int a,int b) {
+		
+		return a+b;
+	}
+	
+	
+}
